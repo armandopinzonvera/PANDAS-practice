c1981bba04ae2464c19cb9f91922df90756714bb
ARMI <armandopinzonvera@gmail.com>
2023-11-02 16:29:36 -0500

    2

--- PANDAS/graficar.py
+++ PANDAS/graficar.py
@@ -2,7 +2,8 @@ import pandas as pd
 
 import matplotlib.pyplot as plt   # toca hacer pip install matplotlib
 
-archivo = "PANDAS\Ccumulacion.xlsx" 
+archivo = "PANDAS\jcumulacion.xlsx" 
+excel_file = "PANDAS\GDB_FAUNA.xlsx"
           
 hoja = "Hoja1"
 
@@ -11,8 +12,16 @@ documento = pd.read_excel(archivo, sheet_name= hoja)
 #propiedades = documento.info()
 
 tabla = pd.read_excel(archivo, sheet_name= hoja, index_col=0, parse_dates=True)
-info_graf = tabla["Jack 2 Mean"].plot() #con esto se grafica
+'''
+tabla.plot() # Grafica todos los valores numericos de la tabla
+info_graf = tabla["Jack 2 Mean"].plot() #con esto se grafica solo esa serie
+'''
 
+
+#https://pandas.pydata.org/docs/getting_started/intro_tutorials/04_plotting.html
+
+#compara dos series
+tabla.plot.scatter(x="Doubletons Mean", y="Uniques Mean", alpha = 0.5)
 
 plt.show()
 
